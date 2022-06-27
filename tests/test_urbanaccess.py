9aecf59fcb706e5a942cb078b0adf66f7b0bd5a1
Ori Hoch <ori@uumpa.com>
2022-06-27 17:39:01 +0300

    CI fixes

--- tests/test_urbanaccess.py
+++ tests/test_urbanaccess.py
@@ -13,6 +13,11 @@ from .config import ALLOW_SLOW_TESTS
 
 @pytest.mark.skipif(not ALLOW_SLOW_TESTS, reason='slow test')
 def test_create_network_fake_gtfs_data():
+    try:
+        import open_bus_stride_api
+        use_proxy_server = True
+    except ImportError:
+        use_proxy_server = False
     date = datetime.date(2022, 6, 21)
     min_lon, min_lat, max_lon, max_lat = 34.875169, 32.078883, 34.8804, 32.085137
     start_hour, end_hour = 8, 9
@@ -20,7 +25,7 @@ def test_create_network_fake_gtfs_data():
     with tempfile.TemporaryDirectory() as tmpdir:
         fake_gtfs_path = os.path.join(tmpdir, 'fake_gtfs')
         stride.urbanaccess.create_fake_gtfs.main(
-            date, start_hour, end_hour, bbox, target_path=fake_gtfs_path, use_proxy_server=True,
+            date, start_hour, end_hour, bbox, target_path=fake_gtfs_path, use_proxy_server=use_proxy_server,
             limit_stop_times=5
         )
         filenames = {filename.split('/')[-1] for filename in glob.glob(f'{fake_gtfs_path}/**/*.txt', recursive=True)}
